060a7bd2c6d8b5ac7ef6d277ce0d819e3699abe9
comettlee <comettlee@naver.com>
2021-09-03 17:52:04 +0900

    상단 메뉴바 - 작업완료 페이지 호출시 보이는 home section - 완료

---
+++ js/main.js
@@ -0,0 +1,31 @@
+//COMMON
+
+// 현재 연도 
+document.querySelector('.this-year').textContent = new Date().getFullYear();
+
+
+// HOME
+
+// HOME SLIDE 애니메이션
+const profileHead = document.querySelector('.home__head');
+const profileInfo = document.querySelector('.home__info');
+
+setTimeout(function (){
+    profileHead.classList.remove('hidden');
+    profileHead.classList.add('show');
+},1000);
+
+setTimeout(function () {
+    profileInfo.classList.remove('hidden');
+    profileInfo.classList.add('show');
+},1500);
+
+
+
+
+// 상단 메뉴 클릭시 해당 View로 이동 (화살표 함수 사용)
+document.querySelector('.main-menu').addEventListener('click', e=>{
+    let scrollToView = '#' + e.target.textContent;
+    document.querySelector(scrollToView).scrollIntoView({behavior : 'smooth'});
+});
+
