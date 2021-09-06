8cc3dafda06e65773502da9f22a1be0923928b09
comettlee <comettlee@naver.com>
2021-09-06 11:42:01 +0900

    main home , profile section 반응형 작업중

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 //COMMON
 
 // 현재 연도 
-document.querySelector('.this-year').textContent = new Date().getFullYear();
+// document.querySelector('.this-year').textContent = new Date().getFullYear();
 
 
 // HOME
@@ -10,27 +10,29 @@ document.querySelector('.this-year').textContent = new Date().getFullYear();
 const profileHead = document.querySelector('.home__head');
 const profileInfo = document.querySelector('.home__info');
 
-setTimeout(function (){
+setTimeout(function () {
     profileHead.classList.remove('hidden');
     profileHead.classList.add('show');
-},1000);
+}, 1000);
 
 setTimeout(function () {
     profileInfo.classList.remove('hidden');
     profileInfo.classList.add('show');
-},1500);
+}, 1500);
 
+// 상단 메뉴바 메뉴리스트 클릭시 보이게
 const mainMenu = document.querySelector('.main-menu');
 
-document.querySelector('.list-icon').addEventListener('click', () =>{
-    mainMenu.classList.toggle('active');
+document.querySelector('.list-icon').addEventListener('click', () => {
+        mainMenu.classList.toggle('active');
 })
 
 
 
 // 상단 메뉴 클릭시 해당 View로 이동 (화살표 함수 사용)
-document.querySelector('.main-menu').addEventListener('click', e=>{
+document.querySelector('.main-menu').addEventListener('click', e => {
     let scrollToView = '#' + e.target.textContent;
-    document.querySelector(scrollToView).scrollIntoView({behavior : 'smooth'});
+    document.querySelector(scrollToView).scrollIntoView({
+        behavior: 'smooth'
+    });
 });
-
